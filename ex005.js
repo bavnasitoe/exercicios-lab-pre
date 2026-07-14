14587c2e1e31e71e8b8372eb308c537b29dd418d
Bávna <bavnasitoe03@gmail.com>
2026-07-14 14:16:37 +0200

    Create ex005.js

---
+++ ex005.js
@@ -0,0 +1,6 @@
+const funcionario = {
+    nome: "Nélio Bila", cargo: "Analista de Sistemas", provincia: "Maputo", departamento: "DIS", activo: true
+}
+
+const {nome, cargo, provincia} = funcionario
+console.log(`O funcionário ${nome} trabalha como ${cargo} em ${provincia}`)
