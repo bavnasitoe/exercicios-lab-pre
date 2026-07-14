75118df63c1ee3f880e78cdc53a2981f47b3e86d
Bávna <bavnasitoe03@gmail.com>
2026-07-14 14:21:30 +0200

    Create ex007.js

---
+++ ex007.js
@@ -0,0 +1,7 @@
+const coordenadas = [
+    -25.9692, 32.5732, "Maputo"
+]
+
+const [latitude, longitude, provincia] = coordenadas
+
+console.log(`Latitude: ${latitude} longitude: ${longitude} Provincia:${provincia}`)
